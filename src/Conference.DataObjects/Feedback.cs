e1e5aff2d0c6bf95b9a124925354eb85377e3449
Dmitriy Ivanov <dmitriy.ivanov@developex.net>
2018-08-10 19:36:24 +0300

    added additional fields

--- src/Conference.DataObjects/Feedback.cs
+++ src/Conference.DataObjects/Feedback.cs
@@ -8,5 +8,9 @@
         public string UserId { get; set; }
         public string SessionId { get; set; }
         public int SessionRating { get; set; }
+        public int Prepeared { get; set; }
+        public int Learnnew { get; set; }
+        public int StayedinFocus { get; set; }
+        public int Expertise { get; set; }
     }
 }
